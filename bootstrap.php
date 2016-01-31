ceb517915b134b6dc7dfaad92147779489128fda
Adrian Cieślak <rettush@gmail.com>
2016-01-31 14:10:34 +0100

    Utworzenie dzialajacego ladowania styli + twiga ;)

--- bootstrap.php
+++ bootstrap.php
@@ -5,16 +5,55 @@ require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'a
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'config.php';
 
 
-try {
-    $pdo = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_LOGIN, DB_PASSWORD);
-    $fpdo = new FluentPDO($pdo);
-} catch (PDOException $e) {
-    $error_message = $e->getMessage();
-    echo $error_message;
-    exit();
+use Assetic\AssetWriter;
+use Assetic\Extension\Twig\TwigFormulaLoader;
+use Assetic\Extension\Twig\TwigResource;
+use Assetic\Factory\LazyAssetManager;
+use Assetic\Factory\AssetFactory;
+use Assetic\AssetManager;
+use Assetic\FilterManager;
+use Assetic\Extension\Twig\AsseticExtension;
+use Symfony\Component\Finder\Finder;
+use Assetic\Filter\LessFilter;
+use Assetic\Filter\LessphpFilter;
+
+$loader = new Twig_Loader_Filesystem('assets/views/');
+$options = array(
+    'cache' => false,
+    'debug' => true
+);
+
+$twig = new Twig_Environment($loader, $options);
+
+$assetManager = new AssetManager();
+$filterManager = new FilterManager();
+//$filterManager->set('less', new LessFilter('/opt/local/bin/node'));
+$filterManager->set('less', new LessphpFilter());
+//asset factory
+$assetFactory = new AssetFactory('assets/');
+$assetFactory->setDebug(false);
+$assetFactory->setAssetManager($assetManager);
+$assetFactory->setFilterManager($filterManager);
+
+$twig->addExtension(new AsseticExtension($assetFactory));
+
+$lazyAssetManager = new LazyAssetManager($assetFactory);
+$lazyAssetManager->setLoader('twig', new TwigFormulaLoader($twig));
+
+$finder = new Finder();
+$finder
+    ->files()
+    ->in('assets')
+    ->exclude('css')
+    ->exclude('js')
+    ->exclude('images')
+    ->name("*.twig");
+
+
+foreach ($finder as $template) {
+    $resource = new TwigResource($loader, $template->getFileName());
+    $lazyAssetManager->addResource($resource, 'twig');
 }
 
-$loader = new Twig_Loader_Filesystem('views');
-$twig = new Twig_Environment($loader, array(
-    'cache' => 'cache',
-));
+$writer = new AssetWriter('.');
+$writer->writeManagerAssets($lazyAssetManager);
